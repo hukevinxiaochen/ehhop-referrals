cec5a979d2c2b6d01d8b1db1f37bcec6a3f5fb4e
Ted Pak <powerpak006@gmail.com>
2012-11-02 15:11:33 -0400

    changed footer, made fixes to phq9 form

--- js/app.js
+++ js/app.js
@@ -1,64 +1,74 @@
-$(document).bind("mobileinit", function () {
-    $.mobile.pushStateEnabled = true;
+$(document).delegate("#phq9", "pageinit", function() {
+  $('#phq9 :radio').click(function() {
+    var total = 0;
+    $('#phq9 :checked').each(function() { total += parseInt($(this).val(), 10); });
+    $('#totalSum').val(total);
+  });
 });
- 
-$(function () {
-    var menuStatus;
- 
-    // Show menu
-    $("a.showMenu").click(function () {
-        if (menuStatus != true) {
-            $(".ui-page-active").animate({
-                marginLeft: "165px",
-            }, 300, function () {
-                menuStatus = true
-            });
-            return false;
-        } else {
-            $(".ui-page-active").animate({
-                marginLeft: "0px",
-            }, 300, function () {
-                menuStatus = false
-            });
-            return false;
-        }
-    });
- 
- 
-    $('#menu, .pages').live("swipeleft", function () {
-        if (menuStatus) {
-            $(".ui-page-active").animate({
-                marginLeft: "0px",
-            }, 300, function () {
-                menuStatus = false
-            });
-        }
-    });
- 
-    $('.pages').live("swiperight", function () {
-        if (!menuStatus) {
-            $(".ui-page-active").animate({
-                marginLeft: "165px",
-            }, 300, function () {
-                menuStatus = true
-            });
-        }
-    });
- 
-    $('div[data-role="page"]').live('pagebeforeshow', function (event, ui) {
-        menuStatus = false;
-        $(".pages").css("margin-left", "0");
-    });
- 
-    // Menu behaviour
-    $("#menu li a").click(function () {
-        var p = $(this).parent();
-        if ($(p).hasClass('active')) {
-            $("#menu li").removeClass('active');
-        } else {
-            $("#menu li").removeClass('active');
-            $(p).addClass('active');
-        }
-    });
-});
- 
+
+
+
+// $(document).bind("mobileinit", function () {
+//     $.mobile.pushStateEnabled = true;
+// });
+//  
+// $(function () {
+//     var menuStatus;
+//  
+//     // Show menu
+//     $("a.showMenu").click(function () {
+//         if (menuStatus != true) {
+//             $(".ui-page-active").animate({
+//                 marginLeft: "165px",
+//             }, 300, function () {
+//                 menuStatus = true
+//             });
+//             return false;
+//         } else {
+//             $(".ui-page-active").animate({
+//                 marginLeft: "0px",
+//             }, 300, function () {
+//                 menuStatus = false
+//             });
+//             return false;
+//         }
+//     });
+//  
+//  
+//     $('#menu, .pages').live("swipeleft", function () {
+//         if (menuStatus) {
+//             $(".ui-page-active").animate({
+//                 marginLeft: "0px",
+//             }, 300, function () {
+//                 menuStatus = false
+//             });
+//         }
+//     });
+//  
+//     $('.pages').live("swiperight", function () {
+//         if (!menuStatus) {
+//             $(".ui-page-active").animate({
+//                 marginLeft: "165px",
+//             }, 300, function () {
+//                 menuStatus = true
+//             });
+//         }
+//     });
+//  
+//     $('div[data-role="page"]').live('pagebeforeshow', function (event, ui) {
+//         menuStatus = false;
+//         $(".pages").css("margin-left", "0");
+//     });
+//  
+//     // Menu behaviour
+//     $("#menu li a").click(function () {
+//         var p = $(this).parent();
+//         if ($(p).hasClass('active')) {
+//             $("#menu li").removeClass('active');
+//         } else {
+//             $("#menu li").removeClass('active');
+//             $(p).addClass('active');
+//         }
+//     });
+// });
+//  
